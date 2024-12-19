5404727769e5136c177558bb6ee87139dd3dce2d
VietIS Anhltv <anhltv@vietis.com.vn>
2024-12-19 18:34:05 +0900

    Them responsive

---
+++ main.js
@@ -0,0 +1,11 @@
+const iconBtn = document.querySelector(".header__toggle")
+const navMenu = document.querySelector(".header-nav")
+
+iconBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+}
+)
+
+document.querySelectorAll(".header-nav-list__lnk").forEach(n => n.addEventListener("click", (){
+    navMenu.classList.remove("active");
+}))
